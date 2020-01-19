86aa1e5c3314f685c8749ce2993e1ce589823d4d
Jacky Tung <Jacky_Tung@htc.com>
2020-01-19 21:49:27 +0800

    redux: select data from store

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Typography from '@material-ui/core/Typography';
 
+import { clearProfile, fetchProfile } from '@/features/Account/profile';
+
 const Profile = () => {
+  const dispatch = useDispatch();
+
+  // on more complex case, use reselect library
+  const profile = useSelector(state => state.profile);
+
+  useEffect(() => {
+    dispatch(fetchProfile());
+
+    return () => dispatch(clearProfile());
+  }, []);
+
   return (
     <>
       <Typography className="text-bold">This is profile page</Typography>
       <div className="d-flex align-center">
         <Typography variant="body1">Name</Typography>
-        <Typography variant="body2">Jacky</Typography>
+        <Typography variant="body2">{profile.name}</Typography>
       </div>
 
       <div className="d-flex align-center">
         <Typography variant="body1">Email</Typography>
-        <Typography variant="body2">jacky@gmail.com</Typography>
+        <Typography variant="body2">{profile.email}</Typography>
       </div>
     </>
   );
